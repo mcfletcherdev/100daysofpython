3de23bdd034746fa6c77b80bb75e3ae5e0811ca3
mcfletcherdev <165359882+mcfletcherdev@users.noreply.github.com>
2025-10-08 10:48:15 +0000

    changes to day 4

---
+++ pay4.py
@@ -0,0 +1,9 @@
+# Create a basic rock paper scissors.
+
+from rich.console import Console
+console = Console()
+from rich.prompt import Prompt
+import questionary
+
+console.rule('[yellow]Welcome to Rock, Paper Scissors![/yellow]')
+choice = questionary []
